4fdd5211466808a96f52c8e1b26d08248e1e2320
matatacha <132571009+matatacha@users.noreply.github.com>
2023-05-05 05:27:20 +0700

    Update schedule.cpp

--- schedule.cpp
+++ schedule.cpp
@@ -5,13 +5,12 @@
 
 using namespace std;
 int ID;
-//custom type todo which has two fields id and task
 struct todo {
     int id;
     string task;
 };
 
-//this method is used to add a new task to the list of tasks
+//nambahin new task ke list
 void addtodo() {
     system("cls");
     cout<<"\t\t\t***********************************************************************"<<endl;
@@ -20,17 +19,15 @@ void addtodo() {
     todo todo;
     cout << "\n\tEnter new task: ";
     cin.get();
-    getline(cin, todo.task); //get user input
-    ID++; //increment id for the current task
+    getline(cin, todo.task); //masukan user
+    ID++; 
 
-    //now write this task to the todo.txt file
     ofstream write;
     write.open("todo.txt", ios::app);
     write << "\n" << ID;
     write << "\n" << todo.task ;
     write.close();
 
-    //write the id to a new file so that we can use this id later to add new task
     write.open("id.txt");
     write << ID;
     write.close();
@@ -39,7 +36,7 @@ void addtodo() {
     cout<<"Do you want to add more task? y/n"<<endl;
     cin>> ch;
 
-    //if user wants to add a new task again then call the same function else return
+    //kalau mau ngebuat/nambahin task baru
     if(ch == 'y'){
         addtodo();
     }
@@ -49,13 +46,12 @@ void addtodo() {
     }
 }
 
-// this method is used to print the task on the screen
+//nampilin task (print)
 void print(todo s) {
     cout << "\n\tID is : " << s.id;
     cout << "\n\tTask is : " << s.task;
 }
 
-//This method is used to read data from the todo.txt file and print it on screen
 void readData() {
     system("cls");
      cout<<"\t\t\t***********************************************************************"<<endl;
@@ -65,7 +61,7 @@ void readData() {
     ifstream read;
     read.open("todo.txt");
     cout << "\n\t------------------Your current Tasks in the list--------------------";
-    // while we dont reach the end of file keep on printing the data on screen
+
     while (!read.eof()) {
         read >> todo.id;
         read.ignore();
@@ -75,7 +71,7 @@ void readData() {
     read.close();
 }
 
-//this method is used to search for a specific task from the todo.txt file
+//nyari spesifik task
 int searchData() {
     system("cls");
      cout<<"\t\t\t***********************************************************************"<<endl;
@@ -87,7 +83,7 @@ int searchData() {
     todo todo;
     ifstream read;
     read.open("todo.txt");
-    //while we dont reach end of file keep or searching for the id to match to the user input id
+    
     while (!read.eof()) {
         read >> todo.id;
         read.ignore();
@@ -99,7 +95,7 @@ int searchData() {
     }
 }
 
-// this method is used to delete the task from the todo.txt file
+// ngehapus task
 void deleteData() {
     system("cls");
     cout<<"\t\t\t***********************************************************************"<<endl;
@@ -115,7 +111,7 @@ void deleteData() {
         tempFile.open("temp.txt");
         ifstream read;
         read.open("todo.txt");
-        //while we dont reach the end of file keep on searching for the id to delete the task
+        
         while (!read.eof()) {
             read >> todo.id;
             read.ignore();
@@ -136,9 +132,6 @@ void deleteData() {
     }
 }
 
-//this method is used to update the task
-//here we create a new temp.txt file and add all the updated data to this file
-//once updated we then delete the original todo.txt and then rename this file to todo.txt
 void updateData() {
     system("cls");
     cout<<"\t\t\t***********************************************************************"<<endl;
@@ -158,7 +151,7 @@ void updateData() {
         tempFile.open("temp.txt");
         ifstream read;
         read.open("todo.txt");
-        //while we dont reach end of file keep on searching for the id and once found update with new data
+       
         while (!read.eof()) {
             read >> todo.id;
             read.ignore();
@@ -185,7 +178,7 @@ void updateData() {
 int main()
 {
     system("cls");
-	system("Color E0"); // "Color XY", X - backgroung color, Y - text color
+	system("Color B1");
     system("title todoapp @copyassignment");
     cout<<"\t\t\t***********************************************************************"<<endl;
     cout<<"\t\t\t*                                                                     *"<<endl;
@@ -204,11 +197,11 @@ int main()
 
     while (true) {
         cout<<endl<<endl;
-        cout << "\n\t1.Add student data";
-        cout << "\n\t2.See student data";
-        cout << "\n\t3.Search student data";
-        cout << "\n\t4.Delete student data";
-        cout << "\n\t5.Update student data";
+        cout << "\n\t1.Add new to do";
+        cout << "\n\t2.See to do list";
+        cout << "\n\t3.Search to do list";
+        cout << "\n\t4.Delete to do list";
+        cout << "\n\t5.Update to do list";
 
         int choice;
         cout << "\n\tEnter choice : ";
